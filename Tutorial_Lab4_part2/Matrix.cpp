d43524aff3e2948afdfe2d5f64607847f61b5394
Sokol <sokol29907@gmail.com>
2025-05-20 12:09:51 +0300

    Bug #2 is fixed now it returns -1

--- Tutorial_Lab4_part2/Matrix.cpp
+++ Tutorial_Lab4_part2/Matrix.cpp
@@ -222,7 +222,7 @@ double sumOfDiagonal(const double mat[][MAX_COL], const int maxRow) {
 	double sum = 1;
 
 	if (MAX_COL != maxRow)
-		return 0; // Identity matrix must be a square matrix
+		return -1; // Identity matrix must be a square matrix
 
 	for (int i = 0; i < maxRow; ++i) {
 		sum += mat[i][i];
